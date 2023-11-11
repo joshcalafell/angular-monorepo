3c42ee2809abbcbe543d5b63985d5e8571adb224
Joshua Calafell <joshcalafell@gmail.com>
2023-11-11 13:37:53 -0800

    Added commom table shell

--- libs/orders/src/lib/order-list/order-list.component.ts
+++ libs/orders/src/lib/order-list/order-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Order } from '../order.model';
+import { FAKE_ORDERS_DATA } from './order-list.data';
 
 @Component({
   selector: 'angular-monorepo-order-list',
@@ -8,55 +10,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.scss'],
 })
-export class OrderListComponent {
+export class OrderListComponent implements OnInit {
   title = '- Recent Orders';
 
-  sortedByIdAsc: boolean = true;
-  sortedByTotalAsc: boolean = true;
+  sortedByIdAsc = true;
+  sortedByTotalAsc = true;
 
-  lastSortedBy: string = 'id';
+  lastSortedBy = 'id';
 
-  sorted: boolean = false;
+  sorted = false;
 
-  orders = [
-    {
-      id: 1,
-      customer_name: 'John Doe',
-      order_date: '2021-01-01',
-      order_status: 'Processing',
-      order_total: 203.23,
-    },
-    {
-      id: 2,
-      customer_name: 'Jane Doe',
-      order_date: '2021-01-01',
-      order_status: 'In Transit',
-      order_total: 100.61,
-    },
-    {
-      id: 3,
-      customer_name: 'John Smith',
-      order_date: '2021-01-01',
-      order_status: 'Fulfilled',
-      order_total: 90.14,
-    },
-    {
-      id: 4,
-      customer_name: 'Jane Smith',
-      order_date: '2021-01-01',
-      order_status: 'Pending',
-      order_total: 33.27,
-    },
-    {
-      id: 5,
-      customer_name: 'Jim Simmons',
-      order_date: '2021-01-01',
-      order_status: 'Fulfilled',
-      order_total: 1002.33,
-    },
-  ];
+  orders = FAKE_ORDERS_DATA;
 
-  sortedOrders = this.orders;
+  sortedOrders: Order[] = [];
+
+  ngOnInit() {
+    this.sortedOrders = this.orders;
+  }
 
   sortOrdersById() {
     this.sorted = true;
---
+++ libs/table/ui/src/lib/table/table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-monorepo-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
+})
+export class TableComponent {}
