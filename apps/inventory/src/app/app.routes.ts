e27db98950764cb9becd704830c1a6c1bf7b52db
Joshua Calafell <joshcalafell@gmail.com>
2023-11-13 13:09:49 -0800

    About page, SCSS, Routing

--- apps/inventory/src/app/app.routes.ts
+++ apps/inventory/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Route } from '@angular/router'
 import { InventoryPageComponent } from './inventory-page/inventory-page.component'
 import { OrdersPageComponent } from './orders-page/orders-page.component'
+import { AboutPageComponent } from './about-page/about-page.component'
 
 export const appRoutes: Route[] = [
 	{
 		path: '',
-		redirectTo: 'inventory',
+		redirectTo: 'about',
 		pathMatch: 'full',
+	},
+	{
+		path: 'about',
+		component: AboutPageComponent,
 	},
 	{
 		path: 'inventory',
