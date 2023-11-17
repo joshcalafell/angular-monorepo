8aedfba3e7224617bf78b492f804486335adff13
Joshua Calafell <joshcalafell@gmail.com>
2023-11-17 07:34:30 -0800

    Specs passing

--- apps/inventory/src/app/components/nav/nav.component.spec.ts
+++ apps/inventory/src/app/components/nav/nav.component.spec.ts
@@ -1,13 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { NavComponent } from './nav.component'
+import { RouterTestingModule } from '@angular/router/testing'
+import { ActivatedRoute } from '@angular/router'
 
 describe('NavComponent', () => {
 	let component: NavComponent
 	let fixture: ComponentFixture<NavComponent>
 
+	const fakeActivatedRoute = {
+		snapshot: {
+			data: {},
+			params: {},
+			queryParams: {},
+			fragment: {},
+			outlet: {},
+		},
+	} as ActivatedRoute
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [NavComponent],
+			imports: [NavComponent, RouterTestingModule.withRoutes([])],
+			providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }],
 		}).compileComponents()
 
 		fixture = TestBed.createComponent(NavComponent)
