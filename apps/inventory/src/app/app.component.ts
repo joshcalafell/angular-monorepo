38e1e089acdb69a66b3697afbb1f4dc196b15447
Joshua Calafell <joshcalafell@gmail.com>
2023-11-17 07:29:23 -0800

    Nav component

--- apps/inventory/src/app/app.component.ts
+++ apps/inventory/src/app/app.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core'
 
 import { UpperCasePipe } from '@angular/common'
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
+import { NavComponent } from './components/nav/nav.component'
 
 @Component({
 	standalone: true,
-	imports: [UpperCasePipe, RouterLink, RouterLinkActive, RouterOutlet],
+	imports: [
+		UpperCasePipe,
+		RouterLink,
+		RouterLinkActive,
+		RouterOutlet,
+		NavComponent,
+	],
 	selector: 'angular-monorepo-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
