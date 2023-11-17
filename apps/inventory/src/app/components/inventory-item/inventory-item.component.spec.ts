1473ef86b3101f34cd95000214c23e52f5f84fe3
Joshua Calafell <joshcalafell@gmail.com>
2023-11-17 09:11:19 -0800

    Tests

--- apps/inventory/src/app/components/inventory-item/inventory-item.component.spec.ts
+++ apps/inventory/src/app/components/inventory-item/inventory-item.component.spec.ts
@@ -1,13 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { InventoryItemComponent } from './inventory-item.component'
+import { RouterTestingModule } from '@angular/router/testing'
+import { ActivatedRoute } from '@angular/router'
 
 describe('InventoryItemComponent', () => {
 	let component: InventoryItemComponent
 	let fixture: ComponentFixture<InventoryItemComponent>
 
+	const fakeActivatedRoute = {
+		snapshot: {
+			data: {},
+			params: {},
+			queryParams: {},
+			fragment: {},
+			outlet: {},
+		},
+	}
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [InventoryItemComponent],
+			imports: [InventoryItemComponent, RouterTestingModule.withRoutes([])],
+			providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }],
 		}).compileComponents()
 
 		fixture = TestBed.createComponent(InventoryItemComponent)
