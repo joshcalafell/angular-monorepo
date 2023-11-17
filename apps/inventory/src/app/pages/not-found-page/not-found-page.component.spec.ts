4d7f87c48f897bd839d810701b9cfaa0b543350a
Joshua Calafell <joshcalafell@gmail.com>
2023-11-17 08:33:44 -0800

    Styles cleanup and more components

--- apps/inventory/src/app/pages/not-found-page/not-found-page.component.spec.ts
+++ apps/inventory/src/app/pages/not-found-page/not-found-page.component.spec.ts
@@ -1,13 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { NotFoundPageComponent } from './not-found-page.component'
-
+import { RouterTestingModule } from '@angular/router/testing'
+import { ActivatedRoute } from '@angular/router'
 describe('NotFoundPageComponent', () => {
 	let component: NotFoundPageComponent
 	let fixture: ComponentFixture<NotFoundPageComponent>
 
+	const fakeActivatedRoute = {
+		snapshot: {
+			data: {},
+			params: {},
+			queryParams: {},
+			fragment: {},
+			outlet: {},
+		},
+	} as ActivatedRoute
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [NotFoundPageComponent],
+			imports: [NotFoundPageComponent, RouterTestingModule.withRoutes([])],
+			providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }],
 		}).compileComponents()
 
 		fixture = TestBed.createComponent(NotFoundPageComponent)
