df01de61c1668b7c2a3a3bfcf095ea72b4d59267
Joshua Calafell <joshcalafell@gmail.com>
2023-11-17 23:51:19 -0800

    Inventory Service

--- apps/inventory/src/app/service/inventory/inventory.service.ts
+++ apps/inventory/src/app/service/inventory/inventory.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { InventoryItem } from './inventory.model'
 
 @Injectable({
 	providedIn: 'root',
 })
 export class InventoryService {
-	constructor() {}
+	$inventory: Observable<InventoryItem[]> = new Observable<InventoryItem[]>()
+
+	constructor() {
+		const inventory = this.fillWithRandomData(10)
+		this.$inventory = new Observable<InventoryItem[]>((observer) => {
+			observer.next(inventory)
+		})
+	}
+
+	fillWithRandomData(count: number) {
+		const inventory: InventoryItem[] = []
+		for (let i = 0; i < count; i++) {
+			inventory.push(
+				new InventoryItem(
+					i,
+					'Item ' + i,
+					'Item ' + i + ' description',
+					Math.floor(Math.random() * 1000),
+					'https://picsum.photos/200/300',
+					new Date(),
+					Math.floor(Math.random() * 5),
+					Math.floor(Math.random() * 100),
+					'In Stock',
+					'10 x 10 x 10',
+					'Paraffin',
+					'10 oz',
+					'Wood',
+					'10 hours'
+				)
+			)
+		}
+
+		return inventory
+	}
 }
