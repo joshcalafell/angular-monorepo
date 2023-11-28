6948d87063f4df9474fbd070122b8c56e34d64b5
Joshua Calafell <joshcalafell@gmail.com>
2023-11-28 15:37:18 -0800

    Inventory page cleanup

--- apps/inventory/src/app/pages/inventory-page/inventory-page.component.ts
+++ apps/inventory/src/app/pages/inventory-page/inventory-page.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common'
-import { Component, OnInit, inject } from '@angular/core'
+import { Component, inject } from '@angular/core'
 
 import { CartComponent } from '@angular-monorepo/cart'
 import { RouterLink } from '@angular/router'
 import { HeaderComponent } from '../../components/header/header.component'
 import { InventoryItemComponent } from '../../components/inventory-item/inventory-item.component'
-import { InventoryItem } from '../../service/inventory/inventory.model'
 import { InventoryService } from '../../service/inventory/inventory.service'
 
 @Component({
@@ -22,14 +21,6 @@ import { InventoryService } from '../../service/inventory/inventory.service'
 	styleUrls: ['./inventory-page.component.scss'],
 	providers: [InventoryService, RouterLink],
 })
-export class InventoryPageComponent implements OnInit {
-	private inventoryService = inject(InventoryService)
-
-	items = [] as InventoryItem[]
-
-	constructor() {}
-
-	ngOnInit(): void {
-		this.items = this.inventoryService.items
-	}
+export class InventoryPageComponent {
+	items = inject(InventoryService).items
 }
