3538da3a2d3fe89a17bdcd9df0a758dd2d6f2a86
Joshua Calafell <joshcalafell@gmail.com>
2024-01-23 21:21:52 -0800

    Hover on buttons, specs, and fixups

--- libs/button/ui/src/lib/button/button.component.spec.ts
+++ libs/button/ui/src/lib/button/button.component.spec.ts
@@ -5,6 +5,14 @@ describe('ButtonComponent', () => {
 	let component: ButtonComponent
 	let fixture: ComponentFixture<ButtonComponent>
 
+	const mockButon = {
+		buttonClass: 'btn btn-primary',
+		buttonText: 'Button',
+		buttonType: 'button',
+		buttonDisabled: false,
+		buttonId: '1',
+	}
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [ButtonComponent],
@@ -12,10 +20,48 @@ describe('ButtonComponent', () => {
 
 		fixture = TestBed.createComponent(ButtonComponent)
 		component = fixture.componentInstance
+		component.buttonClass = mockButon.buttonClass
+		component.buttonText = mockButon.buttonText
+		component.buttonType = mockButon.buttonType
+		component.buttonDisabled = mockButon.buttonDisabled
+		component.buttonId = mockButon.buttonId
+
 		fixture.detectChanges()
 	})
 
 	it('should create', () => {
 		expect(component).toBeTruthy()
+	})
+
+	it('should render button', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('button')).toBeTruthy()
+	})
+
+	it('should render button with text', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('button')?.textContent).toContain('Button')
+	})
+
+	it('should render button with class', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('button')?.className).toContain(
+			'btn btn-primary'
+		)
+	})
+
+	it('should render button with type', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('button')?.type).toContain('button')
+	})
+
+	it('should render button with id', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('button')?.id).toContain('1')
+	})
+
+	it('should render button with disabled', () => {
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('button')?.disabled).toBeFalsy()
 	})
 })
